704378d9395effa541b057d47079a05394093120
marionmorel <marion.mrl@live.fr>
2026-03-28 13:55:18 +0000

    Tasks 12 - 18

--- script.py
+++ script.py
@@ -38,3 +38,28 @@ print(names_to_ages)
 # Task 11
 marina_age = names_to_ages.get("Marina", None)
 print("Marina's age is " + str(marina_age))
+
+# Task 12
+medical_records = {}
+
+# Task 13
+medical_records["Marina"] = {"Age": 27, "Sex": "Female", "BMI": 31.1, "Children": 2, "Smoker": "Non-smoker", "Insurance_cost": 6607.0}
+
+# Task 14
+medical_records["Vinay"] = {"Age": 24, "Sex": "Male", "BMI": 26.9, "Children": 0, "Smoker": "Non-smoker", "Insurance_cost": 3225.0}
+medical_records["Connie"] = {"Age": 43, "Sex": "Female", "BMI": 25.3, "Children": 3, "Smoker": "Non-smoker", "Insurance_cost": 8886.0}
+medical_records["Isaac"] = {"Age": 35, "Sex": "Male", "BMI": 20.6, "Children": 4, "Smoker": "Smoker", "Insurance_cost": 16444.0}
+medical_records["Valentina"] = {"Age": 52, "Sex": "Female", "BMI": 18.7, "Children": 1, "Smoker": "Non-smoker", "Insurance_cost": 6420.0}
+
+# Task 15
+print(medical_records)
+
+# Task 16
+print("Connie's insurance cost is " + str(medical_records["Connie"]["Insurance_cost"]) + " dollars.")
+
+# Task 17
+medical_records.pop("Vinay")
+
+# Task 18
+for name, record in medical_records.items():
+    print(name + " is a " + str(record["Age"]) + " year old " + record["Sex"] + " " + record["Smoker"] + " with a BMI of " + str(record["BMI"]) + " and insurance cost of " + str(record["Insurance_cost"]))
